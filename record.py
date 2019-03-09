8a26da1a1abba17f122ab7942c991fffcfcc7e20
winter-code <arpana11mehta@gmail.com>
2019-03-10 00:54:39 +0530

    Recording files and saving them as .wav

---
+++ record.py
@@ -0,0 +1,41 @@
+import pyaudio
+import wave
+
+
+path= input("Enter directory to store at :")
+for i in range(2):
+	FORMAT = pyaudio.paInt16
+	CHANNELS = 2
+	RATE = 44100
+	CHUNK = 1024
+	RECORD_SECONDS = 1
+	
+	WAVE_OUTPUT_FILENAME = path+"/file"+str(i)+".wav"
+	 
+	audio = pyaudio.PyAudio()
+	 
+	# start Recording
+	print ("recording... SPEAK NOW")
+	stream = audio.open(format=FORMAT, channels=CHANNELS,
+	                rate=RATE, input=True,
+	                frames_per_buffer=CHUNK)
+	
+	frames = []
+	 
+	for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+	    data = stream.read(CHUNK)
+	    frames.append(data)
+	print ("finished recording")
+	 
+	 
+	# stop Recording
+	stream.stop_stream()
+	stream.close()
+	audio.terminate()
+	 
+	waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+	waveFile.setnchannels(CHANNELS)
+	waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+	waveFile.setframerate(RATE)
+	waveFile.writeframes(b''.join(frames))
+	waveFile.close()
