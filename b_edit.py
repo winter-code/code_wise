5b728b39a262bd0e37f30c9c3c1c45353a8f1cf8
Anjali.9621 <anjali.grg98@gmail.com>
2019-03-10 16:01:32 +0530

    final updates before submission

--- b_edit.py
+++ b_edit.py
@@ -1,5 +1,7 @@
 from scipy.io import wavfile
 import matplotlib.pyplot as plt
+from scipy.fftpack import fft
+import numpy as np
 
 path=input("Enter directory : ")
 for i in range(3):
@@ -12,3 +14,13 @@ for i in range(3):
 
 	plt.plot(data)
 	plt.show()
+
+
+
+fs, data = wavfile.read('./audiofiles/file0.wav') # load the data
+a = data.T # this is a two channel soundtrack, I get the first track
+b=[(ele/2**16.)*2-1 for ele in a] # this is 16-bit track, b is now normalized on [-1,1)
+c = fft(b) # calculate fourier transform (complex numbers list)
+d = len(c)//2  # you only need half of the fft list (real signal symmetry)
+plt.plot(abs(c[:(d-1)]),'r') 
+plt.show()
