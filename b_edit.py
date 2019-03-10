40cfe2f85c22022dc9707823448d89fcdc9e0e34
Anjali.9621 <anjali.grg98@gmail.com>
2019-03-10 11:38:03 +0530

    update by Anjali into new_branch2

--- b_edit.py
+++ b_edit.py
@@ -2,8 +2,8 @@ from scipy.io import wavfile
 import matplotlib.pyplot as plt
 
 path=input("Enter directory : ")
-for i in range(2):
-	Input_file= path+"/file"+str(i)+".wav"
+for i in range(3):
+	Input_file= "./"+ path+"/file"+str(i)+".wav"
 	fs, data= wavfile.read(Input_file)
 
 	print(fs)
--- spectogram.py
+++ spectogram.py
@@ -1,7 +1,6 @@
 from scipy.io import wavfile
 import matplotlib.pyplot as plt
 
-print("take file ?")
 filename= input("Enter file name: ")
 floc= "./"+filename+".wav"
 fs, data= wavfile.read(floc)
